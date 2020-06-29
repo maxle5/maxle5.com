e28cfa60f3dca503d707cc0fac95950857d42cc6
Max Lefebvre <max.lefebvre@garvin-allen.com>
2020-06-29 07:36:11 -0300

    styled about me page

--- pages/About.vue
+++ pages/About.vue
@@ -1,18 +1,20 @@
 <template>
-  <div>
-    <h1>About Me</h1>
-    <p>
-      I'm a software developer from Halifax, NS with a passion for learning new
-      tools and technologies. Currently having fun using Vue.JS, .NET Core,
-      PWAs, microservices, and more.
-    </p>
+  <div class="px-4 mb-8">
+    <div class="max-w-3xl bg-white rounded-lg mx-auto mt-4 p-4 lg:mt-8 lg:p-8">
+      <h1 class="text-2xl font-medium mb-2">About Me</h1>
+      <p>
+        I'm a software developer from Halifax, NS with a passion for learning
+        new tools and technologies. Currently having fun using Vue.JS, .NET
+        Core, PWAs, microservices, and more.
+      </p>
 
-    <p>something about contributing to dotnet/runtime</p>
+      <p>something about contributing to dotnet/runtime</p>
 
-    <p>
-      I am excited to continue learning while working in the IT industry and
-      discovering new tools and technologies.
-    </p>
+      <p>
+        I am excited to continue learning while working in the IT industry and
+        discovering new tools and technologies.
+      </p>
+    </div>
   </div>
 </template>
 
