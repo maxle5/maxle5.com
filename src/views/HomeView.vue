e3f3b402da9ef70a6b4d5f2bbec9268e05e116df
Max Lefebvre <max-lefebvre@live.com>
2024-02-05 14:39:08 -0400

    restart from scratch - added vue template project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useCounterStore } from '@/stores/counter';
+import { storeToRefs } from 'pinia';
+
+const store = useCounterStore();
+const { count } = storeToRefs(store)
+
+</script>
+
+<template>
+  <main>
+    <h1>Home {{ count }}</h1>
+    <button @click="store.increment">More</button>
+  </main>
+</template>
