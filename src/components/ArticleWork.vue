ae586e1a84eec13ae8e0ab01269f3219806de8eb
Max Lefebvre <max.lefebvre@garvin-allen.com>
2024-09-25 17:35:23 -0300

    mocked various pages

--- src/components/ArticleWork.vue
+++ src/components/ArticleWork.vue
@@ -2,9 +2,13 @@
   <article>
     <h1>Work</h1>
     * Currently working at Garvin-Allen Solutions as a Software Architect. 
-    * Team lead 
-    * Mentor 
-    * Developer 
-    * Designer
+        * Team lead 
+        * Mentor 
+        * Developer 
+        * Designer
+    * Contribute to open source projects on GitHub.
+        * GlazeWM
+        * Zellij
+        * TODO: dotnet
   </article>
 </template>
