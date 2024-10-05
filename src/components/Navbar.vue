7c312c92ab132da429d261bcc2885915391723d0
Max Lefebvre <max.lefebvre@garvin-allen.com>
2024-10-05 15:59:39 -0300

    added icons, styling, etc.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,27 +2,63 @@
   <nav
     class="display-flex justify-content-between position-fixed px-3 w-100 text-decoration-none"
   >
-    <router-link to="/" id="home" class="py-1 color-white">
+    <router-link
+      to="/"
+      id="home"
+      class="py-1 color-primary hover:color-primary-300"
+    >
       Max Lefebvre
     </router-link>
     <ul class="display-flex p-0 py-0_5 m-0 color-light-500">
       <li class="p-1">
-        <router-link to="/work">Work</router-link>
+        <router-link
+          to="/work"
+          class="color-light-500 hover:color-light display-flex align-items-center"
+        >
+          <mdi-briefcase-outline class="mr-0_5" />
+          Work
+        </router-link>
       </li>
       <li class="p-1">
-        <router-link to="/projects">Projects</router-link>
+        <router-link
+          to="/projects"
+          class="color-light-500 hover:color-light display-flex align-items-center"
+        >
+          <mdi-account-hard-hat-outline class="mr-0_5" />
+          Projects
+        </router-link>
       </li>
       <li class="p-1">
-        <router-link to="/blog">Blog</router-link>
+        <router-link
+          to="/blog"
+          class="color-light-500 hover:color-light display-flex align-items-center"
+        >
+          <mdi-notebook-outline class="mr-0_5" />
+          Blog
+        </router-link>
       </li>
       <li class="p-1">
-        <router-link to="/contact">Contact</router-link>
+        <router-link
+          to="/contact"
+          class="color-light-500 hover:color-light display-flex align-items-center"
+        >
+          <mdi-email-outline class="mr-0_5" />
+          Contact
+        </router-link>
       </li>
     </ul>
   </nav>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {
+  MdiBriefcaseOutline,
+  MdiAccountHardHatOutline,
+  MdiNotebookOutline,
+  MdiEmailOutline,
+} from "@maxle5/mdi-vue3";
+</script>
+
 <style scoped>
 #home {
   font-size: 2.5rem;
@@ -33,7 +69,6 @@ nav {
 }
 
 a {
-  color: var(--color-light500);
   text-decoration: none;
 }
 
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,20 +1,31 @@
 <template>
-  <article>
-    <h1 class="color-primary">Hello, I'm Max.</h1>
-    <p>
-      A software developer based in Halifax, Nova Scotia. I specialize in
-      building enterprise web applications using modern technologies.
-    </p>
-
-    <button
-      type="button"
-      to="/projects"
-      class="bg-primary cursor-pointer border-none display-inline-block py-0_5 px-1 border-radius-2 color-white"
-      @click="$router.push('/projects')"
-    >
-      Projects
-    </button>
+  <article class="display-grid md:grid-cols-2 gap-2">
+    <div class="display-flex flex-direction-column justify-content-center pb-8">
+      <h1 class="color-white mb-1">Hello, I'm Max.</h1>
+      <p>
+        A software developer based in Halifax, Nova Scotia. I specialize in
+        building enterprise web applications using modern technologies.
+      </p>
+      <p>
+        I have over 10 years experience in the industry and have worked with
+        .NET and JavaScript technologies.
+      </p>
+    </div>
+    <aside class="display-flex justify-content-center align-items-center">
+      <img src="/avatar.png" alt="Max Lefebvre" class="w-32-rem h-32-rem" />
+    </aside>
   </article>
 </template>
 
 <script setup lang="ts"></script>
+
+<style lang="css" scoped>
+h1 {
+  font-size: 3rem;
+}
+
+p {
+  font-size: 1.25rem;
+  line-height: 1.65rem;
+}
+</style>
