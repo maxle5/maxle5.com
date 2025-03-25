64fbbb948739c184b5465a51e2fb4b77154d0da8
maxle5 <max.lefebvre@garvin-allen.com>
2025-03-24 21:51:21 -0300

    fixed rollup alias

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@src': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
 })
