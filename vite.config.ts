2813b07ae1b7870020a6af96ed17596e966488e0
maxle5 <max.lefebvre@garvin-allen.com>
2025-03-24 21:55:27 -0300

    removed alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vueRouter from 'unplugin-vue-router/vite'
 import vue from '@vitejs/plugin-vue'
@@ -7,14 +5,5 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vueRouter(),
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@src': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+  plugins: [vueRouter(), vue(), vueDevTools()],
 })
