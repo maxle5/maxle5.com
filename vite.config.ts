d05ca386282a28475cae21b5664ccff5e6ad2783
maxle5 <max.lefebvre@garvin-allen.com>
2026-04-07 13:11:09 -0300

    updated deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
-import vueRouter from 'unplugin-vue-router/vite'
+import vueRouter from 'vue-router/vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vueRouter(), vue(), vueDevTools()],
+  plugins: [
+    vueRouter({
+      dts: 'src/route-map.d.ts',
+    }),
+    vue()],
 })
